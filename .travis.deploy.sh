575b99eaff07507d5b9b610ff3c498d9492cd0ec
Thiago Veronezi <thiago@veronezi.org>
2019-01-19 08:37:45 -0500

    init

---
+++ .travis.deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+function deploy_docker_image () {
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        echo "deploying docker image veronezi/$1:$AUTH_VERSION-b$TRAVIS_BUILD_NUMBER"
+        docker push $1:$AUTH_VERSION-b$TRAVIS_BUILD_NUMBER
+        docker push $1
+    else
+       echo "not building master. we wont deploy the docker images."
+    fi
+}
+
+deploy_docker_image veronezi/auth-jwt
+deploy_docker_image veronezi/auth-facade
+
+echo "binaries uploaded"
---
+++ jwt-auth/src/main/node/src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const fs = require("fs");
+
+const jsonParser = bodyParser.json();
+
+const publicKeyPath = path.resolve(process.env.PUBLIC_KEY_DIR, "auth_rsa.pub");
+const privateKeyPath = path.resolve(process.env.PRIVATE_KEY_DIR, "auth_rsa");
+
+const app = express();
+const port = 3000;
+
+const publicKey = fs.readFileSync(publicKeyPath, "utf8");
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+
+const auth = (username, password, expiresIn) => {
+    if(!username || !username.trim()) {
+        throw "invalid username";
+    }
+    return jwt.sign({
+        upn: username,
+        sub: username,
+        iss: "auth",
+        aud: "auth",
+        groups: ["auth"]
+    }, privateKey, {
+        expiresIn: expiresIn || "24h",
+        algorithm: "RS256"
+    });
+};
+
+const start = () => {
+    app.post("/auth", jsonParser, (req, res) => {
+        try {
+            res.send(auth(req.body.username, req.body.password, req.body.expiresIn));
+        } catch (e) {
+            res.status(401).send(e);
+        }
+    });
+    app.get("/publickey", (req, res) => res.send(publicKey));
+    app.listen(port, () => console.log(`App listening on port ${port}!`));
+};
+
+module.exports = {
+    auth,
+    publicKey,
+    start
+};
