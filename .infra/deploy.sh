8aca5c338940bf677cfdcf7685ce655aca9606bb
Thiago Veronezi <thiago@veronezi.org>
2019-04-30 21:55:36 -0400

    mvn helm

--- .infra/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-helm del --purge auth | true
-helm package auth
-helm install --name auth auth-0.1.0.tgz
--- .infra/dryrun.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-helm install --dry-run --debug auth
-
